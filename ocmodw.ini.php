d6fbf2f24e5143b6c92f7ff92533cff961da40b8
akaunderr <akaunderr@gmail.com>
2024-06-14 15:05:43 +0300

    updated module wrapper

---
+++ ocmodw.ini.php
@@ -0,0 +1,15 @@
+<?php
+
+define('VERSION', '1.0.2');
+define('YEAR', '2023');
+
+define('AUTHOR', 'Andrii Burkatskyi');
+define('EMAIL', 'ocmod.space@gmail.com');
+
+define('MODLINK', 'https://www.ocmod.space/consistent-pricing');
+define('OCMLINK', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=44968');
+define('GITLINK', 'https://github.com/ocmod-space/ocmod-consistent-pricing');
+define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
+
+define('LICENSE', 'https://raw.githubusercontent.com/ocmod-space/ocmod-consistent-pricing/main/EULA.txt');
+
